7108ea49c637cbc02192148eceedd6c3b9c5e8a3
S. Andrew Sheppard <andrew@wq.io>
2016-11-15 16:37:23 -0600

    map examples

--- docs/overview/field-types-ui.js
+++ docs/overview/field-types-ui.js
@@ -1,8 +1,34 @@
-define(['wq/photos'],
-function(photos) {
+define(['wq/photos', 'leaflet', 'wq/map', 'leaflet.draw'],
+function(photos, L, map) {
+
+L.Icon.Default.imagePath = "/css/lib/images";
+
+return main;
 
 function main($elems) {
     photos.run('doc', 'detail', 'field-types');
+    makeMap($elems.find('#point-map')[0], 'marker');
+    makeMap($elems.find('#linestring-map')[0], 'polyline');
+    makeMap($elems.find('#polygon-map')[0], 'polygon');
+}
+
+function makeMap(elem, type) {
+    var m = L.map(elem).setView([45, -93.25], 8);
+    var basemaps = map.createBasemaps();
+    var basemap = basemaps[Object.keys(basemaps)[0]];
+    basemap.addTo(m);
+    var opts = {
+        'circle': false,
+        'marker': false,
+        'polyline': false,
+        'polygon': false,
+        'rectangle': false
+    };
+    opts[type] = {};
+    new L.Control.Draw({
+        'draw': opts,
+        'edit': {'featureGroup': layer}
+    }).addTo(m);
 }
 
 return main;
