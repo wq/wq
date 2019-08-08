5da9b7e880043822af04391537ab72bdedff330b
S. Andrew Sheppard <andrew@wq.io>
2019-08-08 15:30:26 +0900

    use setuptools_scm & markdown readme; python3 only

--- setup.py
+++ setup.py
@@ -6,33 +6,33 @@ A modular framework for citizen science field data collection via offline-capabl
 """
 
 
-def long_description():
-    """Return long description from README.rst if it's present
-    because it doesn't get installed."""
+def readme():
     try:
-        return open(os.path.join(
-            os.path.dirname(__file__), 'README.rst'
-        )).read()
+        readme = open('README.md')
     except IOError:
         return LONG_DESCRIPTION
+    else:
+        return readme.read()
 
 
 setup(
     name='wq',
-    version='1.1.0',
+    use_scm_version=True,
     author='S. Andrew Sheppard',
     author_email='andrew@wq.io',
     url='https://wq.io/',
     license='MIT',
     description=LONG_DESCRIPTION.strip(),
-    long_description=long_description(),
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     install_requires=[
-        'wq.app>=1.1.0,<1.2.0',
-        'wq.core>=1.1.0,<1.2.0',
-        'wq.db>=1.1.0,<1.2.0',
-        'wq.io>=1.1.0,<1.2.0',
-        'wq.start>=1.1.0,<1.2.0',
+        'wq.app>=1.2.0,<1.3.0',
+        'wq.core>=1.1.0,<1.3.0',
+        'wq.db>=1.1.3,<1.3.0',
+        'wq.io>=1.1.0,<1.3.0',
+        'wq.start>=1.1.0,<1.3.0',
     ],
+    python_requires='>=3',
     packages=['wq'],
     namespace_packages=['wq'],
     classifiers=[
@@ -45,9 +45,13 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
         'Framework :: Django',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Text Processing :: Markup :: HTML',
         'Topic :: Scientific/Engineering :: GIS',
@@ -55,5 +59,8 @@ setup(
         'Topic :: Software Development :: Pre-processors',
         'Topic :: Database :: Database Engines/Servers',
         'Topic :: Text Processing :: Markup :: XML',
-    ]
+    ],
+    setup_requires=[
+        'setuptools_scm',
+    ],
 )
