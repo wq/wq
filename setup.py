38003892849649835b87e4057ca92ad6d27d8715
S. Andrew Sheppard <andrew@wq.io>
2019-10-07 15:36:04 +0900

    wq.db 1.2.0b1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'wq.app>=1.2.0,<1.3.0',
         'wq.core>=1.2.0,<1.3.0',
-        'wq.db>=1.1.3,<1.3.0',
+        'wq.db>=1.2.0,<1.3.0',
         'wq.start>=1.1.0,<1.3.0',
     ],
     python_requires='>=3',
