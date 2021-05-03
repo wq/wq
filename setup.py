e8f2ecd0efe3d7cd9d3540c5f699d7d046924d24
S. Andrew Sheppard <andrew@wq.io>
2021-05-03 17:10:05 +0900

    wq.create 1.3.0b1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'wq.app>=1.3.0b1,<1.4.0',
         'wq.build>=1.3.0b1,<1.4.0',
-        'wq.create>=1.3.0a1,<1.4.0',
+        'wq.create>=1.3.0b1,<1.4.0',
         'wq.db>=1.3.0b1,<1.4.0',
     ],
     python_requires='>=3',
